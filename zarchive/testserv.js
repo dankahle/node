c6d71501badce61cdf9abeb4cf28183261e6d500
dan kahle <dan.kahle@gmail.com>
2019-03-01 10:06:40 -0800

    latest

--- zarchive/testserv.js
+++ zarchive/testserv.js
@@ -1,3 +1,20 @@
+var express = require('express'),
+   bodyParser = require('body-parser'),
+   _ = require('lodash');
 
 
-module.exports.count = 5;
+var app = express()
+app.use(bodyParser.json())
+
+app.get('/', (req, res, next) => {
+   res.status(500).send('root');
+})
+
+
+
+
+app.use((req, res, next) => {
+   res.send('not found');
+})
+
+app.listen(3005, function(){console.log('listening on 3000')});
